fd7053d698aebe7b7f549f905e76a1d804af3908
Tim Papich <timothypapich@gmail.com>
2020-07-05 08:56:57 -0400

    add active and contact fields to db

--- api/models/users.js
+++ api/models/users.js
@@ -3,17 +3,11 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  hash: String,
-  salt: String
+  email: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  active: { type: Boolean, required: true, default: false},
+  contacts: { type: Array, required: true },
+  hash: String, salt: String
 });
 
 userSchema.methods.setPassword = function(password) {
@@ -22,6 +16,14 @@ userSchema.methods.setPassword = function(password) {
     .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
     .toString('hex');
 };
+
+userSchema.methods.addContact = function(contact) {
+  const id = contact.ObjectID();
+  if(!userSchema.findById(id))
+    return(err)
+  else
+    this.contacts.append(id);
+}
 
 userSchema.methods.validPassword = function(password) {
   const hash = crypto
