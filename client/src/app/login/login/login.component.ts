c26cc3c8866641c945eddf6a2c06dc9c1d2e377b
mita6978 <mita6978@colorado.edu>
2020-07-12 22:38:05 -0600

    Init Auth Test

--- client/src/app/login/login/login.component.ts
+++ client/src/app/login/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SocialAuthService, FacebookLoginProvider } from 'angularx-social-login';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      console.log(user);
+    });
+  }
+
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
   }
 
 }
