81656ee5d9a4cce2ae01b5e95edb96d6851f7598
jknoblock <jekn0229@colorado.edu>
2020-07-14 22:44:51 -0500

    Modal work

--- client/src/app/chat/make-call/make-call.component.ts
+++ client/src/app/chat/make-call/make-call.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-make-call',
   templateUrl: './make-call.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./make-call.component.scss']
 })
 export class MakeCallComponent implements OnInit {
-  @Input() name;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  openBackDropCustomClass(content) {
+    this.modalService.open(content, {backdropClass: 'light-blue-backdrop'});
+  }
+
+  openWindowCustomClass(content) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
+
+  openSm(content) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openXl(content) {
+    this.modalService.open(content, { size: 'xl' });
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  openScrollableContent(longContent) {
+    this.modalService.open(longContent, { scrollable: true });
+  }
 
   ngOnInit(): void {
   }
