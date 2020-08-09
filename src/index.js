133ba5362c3ed6871d42f51f758b0a0651e7fb8e
mita6978 <mita6978@colorado.edu>
2020-08-08 21:36:52 -0600

    PLease get done

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('phoneCall', (phoneCall) => {
-        console.log('phonecall', phoneCall)
+        console.log('phonecall', phoneCall);
 
         if(phoneCall.phoneCallActive === true) {
             client.video.rooms
